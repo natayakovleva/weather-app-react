07c7908ba127593bf905e7e4fceae4195f2bf0cc
Natalia Yakovleva <yakovlevane29@gmail.com>
2026-03-26 21:48:16 +0200

    added tailwindcss and components Weather and Form

---
+++ src/components/Form.jsx
@@ -0,0 +1,8 @@
+export default function Form() {
+  return (
+    <form>
+      <input type="text" placeholder="Search for your city..." />
+      <button type="submit">Search</button>
+    </form>
+  );
+}
---
+++ src/components/Weather.jsx
@@ -0,0 +1,10 @@
+import Form from "./Form";
+
+export default function Weather() {
+  return (
+    <>
+      <h2 className="text-3xl font-bold underline text-sky-500">Weather</h2>
+      <Form />
+    </>
+  );
+}
